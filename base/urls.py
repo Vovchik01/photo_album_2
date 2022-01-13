dbaec63834b31f89bc71655a615f8b7d3b4b81d5
Vovchik01 <wavilla23@gmail.com>
2022-01-13 16:21:02 +0300

    final commit

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('add_photo/', add_photo, name='add_photo'),
+    path('card_view/<str:pk>/', card_view, name='card_view'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 
 def index(request):
-    context = {}
+    category = request.GET.get('category')
+    if category is None:
+        images = Photo.objects.all()
+    else:
+        images = Photo.objects.filter(category__name=category)
+
+    categories = Category.objects.all()
+    context = {'categories': categories, 'images': images}
     return render(request, 'base/index.html', context)
+
+
+def card_view(request, pk):
+    image = Photo.objects.get(id=pk)
+    context = {'image': image}
+    return render(request, 'base/card_view.html', context)
+
+
+def add_photo(request):
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        data = request.POST
+        image = request.FILES.get('img_upload')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['add_category'] != '':
+            category, created = Category.objects.get_or_create(name=data['add_category'])
+        else:
+            category = None
+        
+        photo = Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        )
+        return redirect('index')
+
+    context = {'categories': categories,}   
+    return render(request, 'base/add_photo.html', context)
